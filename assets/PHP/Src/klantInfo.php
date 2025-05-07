48b7984e8a47912a7656da7eac8001811388966d
Marvin Graaf <marvin.graaf@student.graafschapcollege.nl>
2025-05-07 10:47:56 +0200

    zoekKlant.php en getCustomerByAddress gemaaakt

--- assets/PHP/Src/klantInfo.php
+++ assets/PHP/Src/klantInfo.php
@@ -7,4 +7,12 @@ class Klanten extends Database
         $query = "SELECT * FROM klanten";
         return parent::voerQueryUit($query);
     }
+
+    public function getCustomerByAddress($straat, $postcode, $woonplaats){
+        {
+        $query = "SELECT * FROM klanten WHERE straat = ? AND postcode = ? AND woonplaats = ?";
+        $params = [$straat, $postcode, $woonplaats];
+        return parent::voerQueryUit($query, $params);
+        }
+    }
 }
