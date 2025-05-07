b8b94c62d9a33c916312e0933f7a54ab69f4562a
Jayvano hendriks <jayvano.hendriks@student.graafschapcollege.nl>
2025-05-07 11:28:18 +0200

    tabel kan toegevoegd worden

--- assets/PHP/Src/klantInfo.php
+++ assets/PHP/Src/klantInfo.php
@@ -7,4 +7,14 @@ class Klanten extends Database
         $query = "SELECT * FROM klanten";
         return parent::voerQueryUit($query);
     }
+
+    public function saveCustomer($name , $adres, $phone){
+        if ($name == "" || $adres == "" || $phone == ""){
+            return false;
+        }
+
+        $query = "INSERT INTO klanten (name, telefooonnummer, straat) VALUES (?, ?, ?);";
+        $params = [$name, $phone, $adres];
+        return parent::voerQueryUit($query, $params) > 0;
+    }
 }
