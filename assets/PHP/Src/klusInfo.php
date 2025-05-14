d1451a4444bd5e66ed92c03c09220b2eb579a834
Marvin Graaf <marvin.graaf@student.graafschapcollege.nl>
2025-05-14 09:58:41 +0200

    klusLijst gemaakt en klusInfo werkend gemaakt

--- assets/PHP/Src/klusInfo.php
+++ assets/PHP/Src/klusInfo.php
@@ -4,9 +4,10 @@ require_once('database.php');
 class KlusInfo extends Database
 {
     public function getAllKlussen(){
+        // note: INNER JOIN klus_materialen doet het niet als er niks in klus_materialen zit
         $query = "SELECT * FROM klus AS k
-        INNER JOIN klus_materialen AS km ON k.id = km.klus_id
-        INNER JOIN klanten AS kl ON k.klantid = kl.id";
+        INNER JOIN klanten AS kl
+        ON k.klantid = kl.id";
         return parent::voerQueryUit($query);
     }
 }
