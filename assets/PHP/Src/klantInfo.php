ed3fe24da065651f3cbc9ba003d9e1bd163108d6
Frits <frits.welsing@student.graafschapcollege.nl>
2025-05-14 08:44:43 +0200

    klantinfo saveCustomer functie gefixt

--- assets/PHP/Src/klantInfo.php
+++ assets/PHP/Src/klantInfo.php
@@ -3,25 +3,28 @@ require_once('database.php');
 
 class Klanten extends Database
 {
-    public function getAllCustomers(){
-        $query = "SELECT * FROM klanten";
-        return parent::voerQueryUit($query);
+  public function getAllCustomers()
+  {
+    $query = "SELECT * FROM klanten";
+    return parent::voerQueryUit($query);
+  }
+
+  public function saveCustomer($name, $adres, $phone)
+  {
+    if ($name == "" || $adres == "" || $phone == "") {
+      return false;
     }
 
-    public function saveCustomer($name , $adres, $phone){
-        if ($name == "" || $adres == "" || $phone == ""){
-            return false;
-        }
+    $query = "INSERT INTO klanten (name, telefooonnummer, straat) VALUES (?, ?, ?);";
+    $params = [$name, $phone, $adres];
+    return parent::voerQueryUit($query, $params) > 0;
+  }
 
-        $query = "INSERT INTO klanten (name, telefooonnummer, straat) VALUES (?, ?, ?);";
-        $params = [$name, $phone, $adres];
-        return parent::voerQueryUit($query, $params) > 0;
-
-    public function getCustomerByAddress($straat, $postcode, $woonplaats){
-        {
-        $query = "SELECT * FROM klanten WHERE straat = ? AND postcode = ? AND plaats = ?";
-        $params = [$straat, $postcode, $woonplaats];
-        return parent::voerQueryUit($query, $params);
-        }
+  public function getCustomerByAddress($straat, $postcode, $woonplaats)
+  { {
+      $query = "SELECT * FROM klanten WHERE straat = ? AND postcode = ? AND plaats = ?";
+      $params = [$straat, $postcode, $woonplaats];
+      return parent::voerQueryUit($query, $params);
     }
+  }
 }
