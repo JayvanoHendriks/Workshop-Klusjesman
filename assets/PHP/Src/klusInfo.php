f6301146707f9263e2612da7cac57b9a03d26291
Marvin Graaf <marvin.graaf@student.graafschapcollege.nl>
2025-05-20 10:47:15 +0200

    removed line that caused errors

--- assets/PHP/Src/klusInfo.php
+++ assets/PHP/Src/klusInfo.php
@@ -5,7 +5,6 @@ class KlusInfo extends Database
 {
     public function getAllKlussen(){
         $query = "SELECT * FROM klus AS k
-        INNER JOIN klus_materialen AS km ON k.id = km.klus_id
         INNER JOIN klanten AS kl ON k.klantid = kl.id";
         return parent::voerQueryUit($query);
     }
