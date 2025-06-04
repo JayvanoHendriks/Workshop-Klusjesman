d8d88ffa7f2a341c12fda4d8ce1430f5b1cca9a9
Marvin Graaf <marvin.graaf@student.graafschapcollege.nl>
2025-06-04 09:41:42 +0200

    tijdelijke HTML gemaakt om JQuery beter te doen

--- assets/PHP/Public/KlusKostenBerekenen.php
+++ assets/PHP/Public/KlusKostenBerekenen.php
@@ -7,11 +7,10 @@ $klus = new KlusInfo();
 $totaal = 0.00;
 if (isset($_POST['calc'])) {
     $uurtarief = $_POST['uurtarief'];
-    $minutenGewerkt = $_POST['minuten'];
+    $urenGewerkt = $_POST['uur'];
     $voorrijkosten = $_POST['voorrijkosten'];
     $extraKosten = $_POST['extraKosten'];
-    $minuuttarief = $uurtarief / 60;
-    $totaal = $minuuttarief * $minutenGewerkt;
+    $totaal = $uurtarief * $urenGewerkt;
     $totaal += $voorrijkosten;
     $totaal += $extraKosten;
 }
@@ -28,7 +27,7 @@ $totaalParsed = number_format((float)$totaal, 2, '.', '');
     <form action="" method="post">
         <label>VoorrijKosten: </label><input type="number" name="voorrijkosten" step="0.01" id="" value="<?php if(isset($_POST['calc'])) {echo $voorrijkosten;} ?>"><br>
         <label>Uurtarief: </label><input type="number" name="uurtarief" step="0.01" id="" value="<?php if(isset($_POST['calc'])) {echo $uurtarief;} ?>"><br>
-        <label>Gewerkte minuten: </label><input type="number" name="minuten" id="" value="<?php if(isset($_POST['calc'])) {echo $minutenGewerkt;} ?>"><br>
+        <label>Gewerkte uren: </label><input type="number" name="uur" id="" value="<?php if(isset($_POST['calc'])) {echo $minutenGewerkt;} ?>"><br>
         <label>extra Kosten: </label><input type="number" name="extraKosten" step="0.01" id="" value="<?php if(isset($_POST['calc'])) {echo $extraKosten;} ?>"><br>
         <input type="submit" value="bereken" name="calc">
     </form>
